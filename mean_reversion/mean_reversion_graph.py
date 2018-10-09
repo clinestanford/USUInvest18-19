370088c2cea1f78520c785e12bd2576ac9d43dcc
clinestanford <clinestanford@gmail.com>
2018-10-09 16:00:30 -0600

    added spy.csv

--- mean_reversion/mean_reversion_graph.py
+++ mean_reversion/mean_reversion_graph.py
@@ -2,7 +2,7 @@ import pandas as pd
 import matplotlib.pyplot as plt 
 
 
-df = pd.read_csv("Ford.csv")
+df = pd.read_csv("Spy.csv")
 df = df.set_index("date")
 df = df[["close", "volume"]]
 df = df.reindex(index=df.index[::-1])
@@ -11,43 +11,43 @@ thirty = df.rolling(window=30).mean()
 
 fifty = df.rolling(window=50).mean()
 
-hundred = df.rolling(window=10).mean()
+ten = df.rolling(window=10).mean()
 
 thirty = thirty.rename(columns={"close":"30_close", "volume":"30_volume"})
 
 fifty = fifty.rename(columns={"close":"50_close", "volume":"50_volume"})
 
-hundred = hundred.rename(columns={"close":"100_close", "volume":"100_volume"})
+ten = ten.rename(columns={"close":"100_close", "volume":"100_volume"})
 
 thirty = pd.merge(df, thirty, how="inner", left_index=True, right_index=True)
 
 fifty = pd.merge(df, fifty, how="inner", left_index=True, right_index=True)
 
-hundred = pd.merge(df, hundred, how="inner", left_index=True, right_index=True)
+ten = pd.merge(df, ten, how="inner", left_index=True, right_index=True)
 
 
 thirty = thirty[["close", "30_close"]]
 
 fifty = fifty[["close", "50_close"]]
 
-hundred = hundred[["close", "100_close"]]
+ten = ten[["close", "100_close"]]
 
 thirty = thirty.dropna()
 fifty = fifty.dropna()
-hundred = hundred.dropna()
+ten = ten.dropna()
 
 thirty["over"] = thirty["30_close"]*1.03
 thirty["under"] = thirty["30_close"]*.97
 
-fifty["over"] = fifty["50_close"]*1.05
-fifty["under"] = fifty["50_close"]*.95
+fifty["over"] = fifty["50_close"]*1.03
+fifty["under"] = fifty["50_close"]*.97
 
-hundred["over"] = hundred["100_close"]*1.05
-hundred["under"] = hundred["100_close"]*.95
+ten["over"] = ten["100_close"]*1.03
+ten["under"] = ten["100_close"]*.97
 
 thirty.plot(title="thirty day window")
 fifty.plot(title="fifty day window")
-hundred.plot(title="hundred day window")
+ten.plot(title="ten day window")
 
 plt.show(block=True)
 
